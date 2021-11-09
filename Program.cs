0a3a93042923ef5218b3e507945f890f92fd910a
Сергеев Евгений <evgeniisergeev2002@list.ru>
2021-11-09 19:57:06 +0500

    парсер готов

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using Binance.Net.Objects;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Runtime.Serialization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -14,7 +16,8 @@ namespace BinanceApiDataParser
     {
         static async Task Main(string[] args)
         {
-            var client = new BinanceClient(new BinanceClientOptions(){});
+            var client = new BinanceClient(new BinanceClientOptions() { });
+            ;
             var callResult = await client.Spot.Market.GetKlinesAsync("ETHUSDT"
                 , Binance.Net.Enums.KlineInterval.OneDay,
                 new DateTime(2021, 5, 19),
@@ -22,39 +25,41 @@ namespace BinanceApiDataParser
                 );
             var candles = new List<CandleOHLC>();
             ;
-            foreach(var candle in callResult.Data)
+
+            foreach (var t in callResult.Data)
             {
-                var cndl = new CandleOHLC(candle.OpenTime.Ticks,candle.Open, candle.High, candle.Low, candle.Close );
-                candles.Add(cndl);
+                var candle = new CandleOHLC(t.OpenTime.Ticks / 1000, t.Open, t.High, t.Low, t.Close);
+                candles.Add(candle);
             }
 
-
             //так создаётся объект накопления первые 2 параметра это левая нижняя точка, оставшиеся 2 правая верхняя
-            var a = new Accumulation(candles[0].TimeStamp, candles[0].Low, candles[5].TimeStamp, candles[5].High);
+            //var a = new Accumulation(candles[0].TimeStamp, candles[0].Low, candles[5].TimeStamp, candles[5].High);
+            //string json = JsonSerializer.Serialize(candles);
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            var accum = new List<Accum>(){ new Accum("Base2", "triangle", "", new Settings(5, 6, 7, 8)),
+                                            new Accum("Base2", "triangle", "", new Settings(5, 6, 7, 8)) };
 
 
 
 
 
+            if (File.Exists("data.json")) File.Delete("data.json");
+            var jsonStringCandlesData = JsonSerializer.Serialize(candles.Select(c =>
+                                                new[] { c.TimeStamp, c.Open, c.High, c.Low, c.Close }).ToArray(), options);
+            var jsonStringAccumulationData = JsonSerializer.Serialize(accum, options);
+            ;
+            string pattern = "{\"onchart\": " + jsonStringAccumulationData +",\"chart\": {\"data\": "+jsonStringCandlesData+"}}";
 
-
-            ////string json = JsonSerializer.Serialize(candles);
-            //var options = new JsonSerializerOptions
-            //{
-            //    WriteIndented = true,
-            //    IncludeFields = true,
-            //};
-
-            //if (File.Exists("data.json"))   File.Delete("data.json");
-
-            //var jsonString = JsonSerializer.Serialize(candles.ToArray(),options);
-            //File.WriteAllText("data.json", jsonString);
-            //Console.WriteLine("Data has been saved to file");
+            File.WriteAllText("data.json", pattern);
+            Console.WriteLine("Data has been saved to file");
 
             Console.ReadKey();
         }
     }
-
+    [DataContract]
     public class Accumulation
     {
         double VolumeLevel = 0;
@@ -69,6 +74,52 @@ namespace BinanceApiDataParser
             Price1 = low;
             TimeStamp2 = timeStamp2;
             Price2 = high;
+        }
+    }
+
+
+    public class Accum
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        [JsonPropertyName("data")]
+        public string Data { get; set; }
+        [JsonPropertyName("settings")]
+        public Settings Settings { get; set; }
+
+        public Accum(string name, string type, string data, Settings settings)
+        {
+            Name = name;
+            Type = type;
+            Data = data;
+            Settings = settings;
+        }
+    }
+    public class Settings
+    {
+        [JsonPropertyName("Wind")]
+        public long T1 { get; set; }
+        [JsonPropertyName("t1")]
+        public decimal Price1 { get; set; }
+        [JsonPropertyName("price1")]
+        public long T2 { get; set; }
+        [JsonPropertyName("t2")]
+        public decimal Price2 { get; set; }
+        [JsonPropertyName("price2")]
+        public string Color { get; set; }
+        [JsonPropertyName("zIndex")]
+        public int ZIndex { get; set; }
+
+        public Settings(int time1, int price1, int time2, int price2, string color= "#27d588", int zIndex=0)
+        {
+            T1 = time1;
+            Price1 = price1;
+            T2 = time2;
+            Price2 = price2;
+            Color = color;
+            ZIndex = zIndex;
         }
     }
     public class CandleOHLC
@@ -87,6 +138,4 @@ namespace BinanceApiDataParser
         public decimal Low { get; set; }
         public decimal Close { get; set; }
     }
-
-
 }
