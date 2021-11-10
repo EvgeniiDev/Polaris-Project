392246b5001b8be97127b8c3c05af36c853d1c25
Akergez <ragusseven@gmail.com>
2021-11-10 19:12:30 +0500

    Add Server

--- Admin/AdminConnector.cs
+++ Admin/AdminConnector.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SuperSocket.WebSocket.Server;
 
 namespace BinanceApiDataParser.Admin
 {
     class AdminConnector
     {
+        public static void Server()
+        {
+            var host = WebSocketHostBuilder.Create()
+                .UseWebSocketMessageHandler(
+                    async (session, message) =>
+                    {
+                        await session.SendAsync(message.Message);
+                    }
+                )
+                .ConfigureAppConfiguration((hostCtx, configApp) =>
+                {
+                    configApp.AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        { "serverOptions:name", "TestServer" },
+                        { "serverOptions:listeners:0:ip", "Any" },
+                        { "serverOptions:listeners:0:port", "4040" }
+                    });
+                })
+                .ConfigureLogging((hostCtx, loggingBuilder) =>
+                {
+                    loggingBuilder.AddConsole();
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
     }
 }
