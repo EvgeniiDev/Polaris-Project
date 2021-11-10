1fcbf88e7e2139a71e447a6e8941b91f2c9df080
Сергеев Евгений <evgeniisergeev2002@list.ru>
2021-11-10 20:04:52 +0500

    добавил MySql ,добавил метод GetPairs

--- Program.cs
+++ Program.cs
@@ -13,23 +13,19 @@ namespace BinanceApiDataParser
     {
         static async Task Main(string[] args)
         {
-            //var dataParser = new DataParser();
-            //var candles = await dataParser.GetCandles("ETHUSDT" , Binance.Net.Enums.KlineInterval.FiveMinutes,
-            //                                    new DateTime(2021, 5, 19),new DateTime(2021, 10, 1));
+            var dataParser = new DataParser();
+            var candles = await dataParser.GetCandles("ETHUSDT", Binance.Net.Enums.KlineInterval.FiveMinutes,
+                                                new DateTime(2021, 5, 19), new DateTime(2021, 10, 1));
 
-            //var accumulations = new List<Accumulation>(){ new Accumulation(candles[0].TimeStamp, candles[0].Low, candles[5].TimeStamp
-            //                                                    , candles[5].High, AccumulationType.Rectangle),
-            //                              new Accumulation(candles[0].TimeStamp, candles[0].Low, candles[5].TimeStamp
-            //                                                    , candles[5].High, AccumulationType.Rectangle)};
+            var accumulations = new List<Accumulation>(){ new Accumulation(candles[0].TimeStamp, candles[0].Low, candles[5].TimeStamp
+                                                                , candles[5].High, AccumulationType.Rectangle),
+                                          new Accumulation(candles[0].TimeStamp, candles[0].Low, candles[5].TimeStamp
+                                                                , candles[5].High, AccumulationType.Rectangle)};
 
-            //DataExport.WriteJson(candles, accumulations, "data.json");
+            DataExport.WriteJson(candles, accumulations, "data.json");
             Console.WriteLine("Data has been saved to file");
-
-            // Получить объект Connection подключенный к DB.
-          
-
             Console.ReadKey();
         }
-       
+
     }
 }
