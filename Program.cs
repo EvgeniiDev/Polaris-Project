634106c6b3f4540f7e747e25d899e7e24219559f
Сергеев Евгений <evgeniisergeev2002@list.ru>
2021-11-11 18:51:32 +0500

    aboba

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
-using TradeBot.Admin;
 using TradeBot.Data;
 
 namespace TradeBot
