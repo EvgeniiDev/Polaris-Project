20284008b6d0a3442a9f44b21a7a51f493ce1831
Сергеев Евгений <evgeniisergeev2002@list.ru>
2021-11-11 17:33:01 +0500

    rjvbnt ybrbnt

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using Binance.Net;
-using Binance.Net.Objects;
-using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
@@ -19,7 +16,7 @@ namespace TradeBot
             var candles = await dataParser.GetCandles("ETHUSDT", Binance.Net.Enums.KlineInterval.OneDay,
                                                 new DateTime(2021, 5, 19), new DateTime(2021, 10, 1));
 
-            var accumulations = new List<Accumulation>();// BoxDetectionAlgoritm.FindBoxes(candles).Take(10).ToList();
+            var accumulations = BoxDetectionAlgoritm.FindBoxes(candles).Take(10).ToList();
             ;
             DataExport.WriteJson(candles, accumulations, "data.json");
             Console.WriteLine("Data has been saved to file");
