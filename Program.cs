5c71fa15db120bb9b73aef5409bf57f8e9e81ee8
Сергеев Евгений <evgeniisergeev2002@list.ru>
2022-03-05 22:02:10 +0500

    /

--- Program.cs
+++ Program.cs
@@ -12,12 +12,17 @@ namespace TradeBot
         static async Task Main(string[] args)
         {
             var dataParser = new BinanceConnector();
-            var candles = await dataParser.GetCandles("ETHUSDT", KlineInterval.OneDay,
-                                                new DateTime(2014, 8, 24), new DateTime(2022, 2, 15));
+           // var candles = await dataParser.GetCandles("ETHUSDT", KlineInterval.OneDay,
+           //                                     new DateTime(2014, 8, 24), new DateTime(2022, 2, 15));
             //var candless = new List<Candle>();
             //var zigzag = ZigZag.CalculatePriceStructLight(candles, 1);
             var zigzag = ZigZag.CalculateZigZag(candles, 5m);
             var accumulations = SliceAlgorithm.FindBoxes(candles).ToList();
+
+            var dataParser = new DataParser();
+            var candles = await dataParser.GetCandles("ETHUSDT", Binance.Net.Enums.KlineInterval.OneDay,
+                                                new DateTime(2020, 5, 19), new DateTime(2021, 10, 1));
+
 
             //JoinBoxes(accumulations);
             //var allowedPair =  new [] { "BTCUSDT", "ETHUSDT", "LTCUSDT", "XMRUSDT", "XMRBUSD" };
@@ -30,8 +35,11 @@ namespace TradeBot
             //a.Run();
             //Thread.Sleep(15000);
             //a.Exit();
-
+            var zigzag = ZigZag.GetZigZagDot(ZigZag.CalculateZigZag(candles, 5), candles);
             var segments = new TrendDetector().TrendDetect(zigzag);
+            //var zigzag = ZigZag.CalculatePriceStructLight(candles,1);
+            var zigzag = ZigZag.GetZigZagDot(ZigZag.CalculateZigZag(candles,5), candles);
+
             ;
             Export.WriteJson(candles, accumulations, zigzag, null,segments, @"C:\Users\user\Desktop\tvjs-xp-main\src\apps", "data.json");
             Console.WriteLine("Data has been saved to file");
